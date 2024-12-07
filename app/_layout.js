419bac76b179497eafbc8507c2be144a63ca4a75
Angel Fco <angel02fco@gmail.com>
2024-12-07 14:13:43 -0600

    actualizacion de la app movil

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,6 @@
 import { Redirect } from "expo-router";
 
-export default function Index() {
+export default function Layout() {
   // Redirige a la pantalla de autenticación o a Drawer
   return <Redirect href="/auth/Splash" />;
 }
